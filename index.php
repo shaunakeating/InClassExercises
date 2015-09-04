d723995a8fe211a0efd7f8554aaf0df06241cf49
shaunakeating <skeating@hawkmail.newpaltz.edu>
2015-09-04 15:57:59 +0000

    more bootstrap experimentation

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 
     <!-- Bootstrap -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    		<link rel="stylesheet" href="stylesheet.css">
+    		
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -24,28 +27,133 @@
   
     
         <div class="container">
-              <h1>Hello, world!</h1>
-          <div class="row">
+              <h1>Some Bootstrap Experimentation</h1>
+              
+              <nav>
+				<ul class="nav nav-tabs">
+  					<li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+  					<li><a data-toggle="tab" href="#tab1">Menu 1</a></li>
+  					<li><a data-toggle="tab" href="#tab2">Menu 2</a></li>
+  					<li><a data-toggle="tab" href="#tab3">Menu 3</a></li>
+				</ul>
+			</nav>
+
+			<div class="container">
+				
+				<div class="tab-content">
+  					<div id="home" class="tab-pane fade in active">
+    					<h3>HOME</h3>
+                <div class="row">
             
-            <div class="col-sm-4">
-            <a href="google.com" target="_blank" class="btn btn-danger inline"><i class="glyphicon glyphicon-arrow-right"></i> Go</a>
-            <a href="google.com" target="_blank" class="btn btn-success inline"><i class="glyphicon glyphicon-arrow-right"></i> Go  <span class="badge">6</span></a>
-           </div>
+             <div class="col-sm-4">
+              <a href="google.com" target="_blank" class="btn btn-danger inline"><i class="glyphicon glyphicon-arrow-right"></i> Go</a>
+              <a href="google.com" target="_blank" class="btn btn-success inline"><i class="glyphicon glyphicon-arrow-right"></i> Go  <span class="badge">6</span></a>
+             </div>
            
-           <div class="col-sm-8">
-             <div class="progress">
-              <div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 10%">
-                <span class="sr-only">60% Complete (warning)</span>
+             <div class="col-sm-8">
+               <div class="progress">
+                <div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 10%">
+                  <span class="sr-only">60% Complete (warning)</span>
+                </div>
               </div>
             </div>
-          </div>
            
+            </div>
           </div>
-        </div>
-        
-            <div class="container">    
+  					
+  					<div id="tab1" class="tab-pane fade">
+   						 <div class="jumbotron">
+                  <h1>I AM A JUMBOTRON</h1>
+                      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+                </div>
+  					</div>
+  					
+ 					 <div id="tab2" class="tab-pane fade">
+ 					       					<h3>Alert Samples</h3>
+ 					  <div class="row">
+ 					    
+ 					    <div class="col-sm-8">  
+    					      <p>Lorem ipsum dolor sit amet, clita mollis aeterno ei mel, sit tacimates tincidunt cotidieque ne, sea an copiosae referrentur. Tacimates efficiantur at qui, unum epicuri appellantur quo ut. Ad graeci ocurreret quo. Per ea modus insolens disputationi, enim novum principes ei his.
 
-</div>
+Vix idque postulant ad, duo ei numquam ocurreret. <br> Id vim utinam ubique dissentiunt, cu his quodsi detracto. Ridens putant mei id, vis eu graece commune. Ferri assentior definiebas id his. Etiam inani putant at pro, congue imperdiet appellantur cu usu.
+
+His cu nominati repudiare, eos at labitur intellegat vituperatoribus. <br> Quot saepe perfecto quo cu, eu has dolorem gubergren persecuti. Mei an ferri doming. Postea salutatus no vix, feugiat constituam vel ea. His cu sumo patrioque ullamcorper, tamquam eloquentiam id pro. Ius tantas singulis constituto at, mei eu decore veritus postulant. Vel eu corpora blandit albucius, legimus dignissim mel ut, ex libris nostrum his.
+                    </p>
+                  </div>
+ 					    
+    				  <div class="col-sm-4">  
+    					<div class="alert alert-success" role="alert">Nice One!</div>
+              <div class="alert alert-info" role="alert">Hey, here's a fact for you.</div>
+              <div class="alert alert-warning" role="alert">Check yourself before you wreck yourself</div>
+              <div class="alert alert-danger" role="alert">It's too late!</div>
+  					</div>
+  					
+  					</div>
+  					</div>
+  					
+  					 	<div id="tab3" class="tab-pane fade">
+    					<h3>Grid Test</h3>
+    					
+    					  <div class="row">
+    					    
+    					    <div class="col-sm-2">  
+    					      <a href="#" class="thumbnail">
+                      <img src="https://s-media-cache-ak0.pinimg.com/236x/78/72/85/787285725daa071f32ef52d08765fc3a.jpg" alt="zig-zag pattern">
+                    </a>
+                  </div>
+                  
+                 <div class="col-sm-2">  
+    					      <a href="#" class="thumbnail">
+                      <img src="http://www.rgraph.net/images/pattern2.jpg" alt="circles pattern">
+                    </a>
+                  </div>
+                  
+                <div class="col-sm-2">  
+    					      <a href="#" class="thumbnail">
+                      <img src="http://stylegerms.com/wp-content/uploads/2013/06/pattern-family-01-single1.jpg" alt="triangles pattern">
+                    </a>
+                  </div>
+                <div class="col-sm-2">  
+    					      <a href="#" class="thumbnail">
+                      <img src="https://cms-assets.tutsplus.com/uploads/users/117/posts/20837/final_image/final2.jpg" alt="watercolor pattern">
+                    </a>
+                  </div>
+                  
+                <div class="col-sm-2">  
+    					      <a href="#" class="thumbnail">
+                      <img src="http://4vector.com/i/free-vector-beautiful-background-patterns-vector_017863_Seamless%20Pattern5.jpg" alt="paisley pattern">
+                    </a>
+                  </div>
+                  
+                <div class="col-sm-2">  
+    					      <a href="#" class="thumbnail">
+                      <img src="http://payload217.cargocollective.com/1/5/180361/6652046/chicken_feet_pattern_fullpage_small%20copy.jpg" alt="lines pattern">
+                    </a>
+                  </div>
+                  
+    					  </div>
+    					  
+    					  <div class="row">
+    					    <div class="col-sm-10">  
+    					      <a href="#" class="thumbnail">
+                      <img src="http://stylonica.com/wp-content/uploads/2014/02/Best-Covers-for-Facebook-Timeline-31.jpg" alt="sunflower picture">
+                    </a>
+                  </div>
+                  
+                  <div class="col-sm-2">  
+    					      <a href="#" class="thumbnail">
+                      <img src="http://4vector.com/i/free-vector-beautiful-background-patterns-vector_017863_Seamless%20Pattern5.jpg" alt="paisley pattern">
+                    </a>
+                  </div>
+    					    
+    					    
+    					  </div>
+    					  
+  					</div>
+  					
+				</div>
+			</div>
+           
 
 
 <!-- SCRIPT CITY -->
